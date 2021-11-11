7ff290184f6b91deb9a08e92c3cae545ef2ca477
Saarang Rajguru <saarang.rajguru@protonmail.ch>
2021-11-11 09:40:34 +0530

    download button

--- script.js
+++ script.js
@@ -8,12 +8,17 @@ function handleImage(e) {
     reader.onload = function(event) {
         var img = new Image();
         img.onload = function() {
-            var hRatio = canvas.width / img.width    ;
-            var vRatio = canvas.height / img.height  ;
-            var ratio  = Math.min ( hRatio, vRatio );
-            ctx.drawImage(img, 0,0, img.width, img.height, 0,0,img.width*ratio, img.height*ratio);
+            var hRatio = canvas.width/img.width;
+            var vRatio = canvas.height/img.height;
+            var ratio  = Math.min (hRatio, vRatio);
+            ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width*ratio, img.height*ratio);
         }
         img.src = event.target.result;
     }
     reader.readAsDataURL(e.target.files[0]);
+}
+
+downloadImage = function(el) {
+    var image = canvas.toDataURL("image/jpg");
+    el.href = image;
 }
