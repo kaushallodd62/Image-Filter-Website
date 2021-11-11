ef85909358dedc37c30c52f0b819ceab16517856
Kaushal Lodd <kaushallodd62@gmail.com>
2021-11-11 21:17:00 +0300

    final

--- Code/script.js
+++ Code/script.js
@@ -1,28 +1,12 @@
 var fileInput = document.getElementById('fileInput');
-fileInput.addEventListener('change', handleImage, false);
-var canvas = document.getElementById('imageCanvas');
-var ctx = canvas.getContext('2d');
 var footer = document.getElementById('footer');
+var image = document.getElementById('output');
+var controls = document.getElementById('controls');
 
-function handleImage(e) {
-    var reader = new FileReader();
-    reader.onload = function(event) {
-        var img = new Image();
-        img.onload = function() {
-            // clear canvas
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-            // calculate ratios to scale the images down to fit in the canvas
-            var hRatio = canvas.width/img.width;
-            var vRatio = canvas.height/img.height;
-            var ratio  = Math.min (hRatio, vRatio);
-            var xOffset = canvas.width/2-(img.width*ratio)/2
-            ctx.drawImage(img, 0, 0, img.width, img.height, xOffset, 0, img.width*ratio, img.height*ratio);
-        }
-        img.src = event.target.result;
-    }
-    reader.readAsDataURL(e.target.files[0]);
-    footer.style.visibility = 'visible';
+function loadImage(event) {
+    image.src = URL.createObjectURL(event.target.files[0]);
+    image.style.visibility = 'visible';
+    controls.style.visibility = 'visible';
 }
 
 var brightness = 100;
@@ -59,7 +43,7 @@ const value9 = document.getElementById("sepia");
 
 //Update filters
 function updateFilters() {
-    ctx.filter = "brightness(" + brightness + "%) contrast(" + contrast + "%) saturate(" + saturate + "%) grayscale(" + grayscale + "%) invert(" + invert +
+    image.style.filter = "brightness(" + brightness + "%) contrast(" + contrast + "%) saturate(" + saturate + "%) grayscale(" + grayscale + "%) invert(" + invert +
     "%) hue-rotate(" + huerotate + "deg) blur(" + blur + "px) opacity(" + opacity + "%) sepia(" + sepia + "%)";
 }
 
@@ -233,9 +217,3 @@ slider9.addEventListener("focus", function() {
 slider9.addEventListener("blur", function() {
     value9.style.visibility = "hidden";
 });
-
-// function for download link
-function downloadImage(el) {
-    var image = canvas.toDataURL("image/jpg");
-    el.href = image;
-}
